57f38723f45e16d2404da05b209260af53243f3b
Oksana Kyryliuk <coksanna@gmail.com>
2022-12-07 16:18:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
-import debounce from 'lodash.debounce';
-import { Notify } from 'notiflix';
+// import './css/styles.css';
+// import { fetchCountries } from './fetchCountries';
+// import debounce from 'lodash.debounce';
+// import { Notify } from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
